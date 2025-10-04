5a47d152f9de6f45240bb6bd612b996cf9d91cf0
CommandD1 <101664873+CommandD1@users.noreply.github.com>
2025-10-04 22:02:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,45 @@ function toVers(ref) {
   book = (prefix ? prefix + " " : "") + book;
   return [book,chapter!==undefined?parseInt(chapter):undefined,verse!==undefined?parseInt(verse):undefined];
 }
+function updateTable(rows){
+  document.getElementById("table").innerHTML = `
+    <tr>
+        <th>Start</th>
+        <th>Ende</th>
+        <th>Buch</th>
+        <th>Seite</th>
+        <th>Kreis</th>
+        <th>Datum</th>
+    </tr>
+    ${rows.join("")}
+    `;
+}
+function zeileHinzufuegen(neueEntry,rows){
+      const row = `
+        <tr>
+            <td>${neueEntry.start}</td>
+            <td>${neueEntry.ende}</td>
+            <td>${neueEntry.buch}</td>
+            <td>${neueEntry.seite}</td>
+            <td>${neueEntry.kreis}</td>
+            <td>${new Date(neueEntry.datum).toLocaleDateString()}</td>
+        </tr>
+        `;
+        rows.push(row);
+    }
 function update() {
     const input = document.getElementById("input").value;
+    if(input===""){
+      let rows = [];
+      data.forEach(entry =>  zeileHinzufuegen(entry,rows))
+      updateTable(rows)
+      return
+    }
     let verse = toVers(input)
     if(verse===null)return
     const [buch, kapitel, vers] = verse
     let rows = [];
+    
     data.forEach(entry => {
         const [startBuch, startKapitel, startVers] = toVers(entry.start);
         const [endeBuch, endeKapitel, endeVers] = toVers(entry.ende);
@@ -29,30 +62,10 @@ function update() {
                 if(kapitel===endeKapitel&&vers>endeVers)return;
             }
         }
-        const row = `
-        <tr>
-            <td>${entry.start}</td>
-            <td>${entry.ende}</td>
-            <td>${entry.buch}</td>
-            <td>${entry.seite}</td>
-            <td>${entry.kreis}</td>
-            <td>${new Date(entry.datum).toLocaleDateString()}</td>
-        </tr>
-        `;
-        rows.push(row);
+        zeileHinzufuegen(entry,rows)
     });
-    document.getElementById("table").innerHTML = `
-    <tr>
-        <th>Start</th>
-        <th>Ende</th>
-        <th>Buch</th>
-        <th>Seite</th>
-        <th>Kreis</th>
-        <th>Datum</th>
-    </tr>
-    ${rows.join("")}
-    `;
-
+    updateTable(rows)
 }
+
 
 
